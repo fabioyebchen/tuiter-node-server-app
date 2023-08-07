85d7d616ea8a2c596b327fe10345553732e19a0c
Fabio Chen <59851467+fabioyebchen@users.noreply.github.com>
2023-08-07 19:23:36 -0400

    node server a5

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
+
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+TuitsController(app);
+HelloController(app);
+UserController(app);
+AuthController(app);
+
+app.listen(port);
